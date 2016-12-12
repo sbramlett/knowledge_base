e7d9fa5b552e5f3bc71e3ab90e908dcf4823c2dc
Stephen Bramlett <stephenbramlett@gmail.com>
2016-12-12 18:58:35 +0000

    Setup Articles index loop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :find_article, only: [:show]
     def index
+        @articles = Article.all.order("created_at DESC")
     end
     
     def show
