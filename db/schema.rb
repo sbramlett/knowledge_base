a0d7220516f5d6e4039b09af4596a117984b7c1d
Stephen Bramlett <stephenbramlett@gmail.com>
2016-12-12 21:46:11 +0000

    Add Categories to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212193209) do
+ActiveRecord::Schema.define(version: 20161212202454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,17 @@ ActiveRecord::Schema.define(version: 20161212193209) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
